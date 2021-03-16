e01f244bc88b38488672bef565a0ed291b85a15d
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2021-03-16 22:47:48 +0100

    smaller fixes

--- threadpool.h
+++ threadpool.h
@@ -10,70 +10,54 @@
 
 class ThreadPool {
 public:
-    //std::thread* _threads;
-    std::vector<std::thread> _threads;
+	std::vector<std::thread> _threads;
+	std::vector<std::function<void()>> _queue;
+	std::condition_variable cv;
+	std::mutex mut;
+	bool isShuttingDown = false;
 
-    std::vector<std::function<void()>> _queue;
-    std::condition_variable cv;
-    std::mutex mut;
-    bool isShuttingDown = false;
+	void ThreadFunction() {
+		std::function<void()> function;
+		while (true) {
+			{
+				std::unique_lock<std::mutex> lock(mut);
+				cv.wait(lock, [&] { return isShuttingDown || !_queue.empty(); });
+				if (isShuttingDown)
+					return;
+				function = _queue.front();
+				_queue.erase(_queue.begin());
+			}
+			function();
+		}
+	}
 
-    void ThreadFunction() {
-        std::function<void()> function;
-        while (!isShuttingDown) {
+	~ThreadPool() {
+		isShuttingDown = true;
+		cv.notify_all();
+		for (size_t i = 0; i < _threads.size(); i++)
+		{
+			_threads[i].join();
+		}
+	}
 
-            std::unique_lock<std::mutex> lock(mut);
-            cv.wait(lock, [&] { return isShuttingDown || !_queue.empty(); });
-            if (isShuttingDown)
-                return;
-            function = _queue.front();
-            _queue.erase(_queue.begin());
-            function();    
-        }
-    }
+	//used by main (you are free to change here and in main)
+	ThreadPool(size_t threads) {
+		for (size_t i = 0; i < threads; i++)
+		{
+			_threads.push_back(std::thread([&]() { (ThreadFunction)(); }));
+		}
+	}
 
-    ~ThreadPool() {
-        isShuttingDown = true;
-        cv.notify_all();
-        for (size_t i = 0; i < _threads.size(); i++)
-        {
-            _threads[i].join();
-        }      
-    }
+	template<class F, class... Args>
+	auto enqueue(F&& f, Args&&... args) {
+		auto task = std::make_shared<std::packaged_task<decltype(f(args...))()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+		std::unique_lock<std::mutex> lock(mut);
+		std::function<void()> wrappedTask = [task]() { (*task)(); };
 
-    //used by main (you are free to change here and in main)
-    ThreadPool(size_t threads) {
-        for (size_t i = 0; i < threads; i++)
-        {
-            _threads.push_back(std::thread([&]() { (ThreadFunction)(); }));
-        }
-    }
-
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) {
-
-        //if there is not enough threads ready then put the function in the function queue
-        //using return_type = std::invoke_result_t(F, Args...);
-        auto task = std::make_shared<std::packaged_task<decltype(f(args...))()>>(std::bind(std::forward<F>(f),std::forward<Args>(args)...));
-
-        std::unique_lock<std::mutex> lock(mut);
-        std::function<void()> wrappedTask = [task]() { (*task)(); };
-        _queue.push_back(wrappedTask);
-        cv.notify_one();
-        return task->get_future(); ;
-    }
-
-    //template<class F, class... Args>
-    //auto packaged_task(F&& f, Args&&... args) {
-    //    //auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-    //    auto task = std::make_shared
-    //        <std::packaged_task<return_type()>>
-    //        (
-    //            std::bind(std::forward<F>(f),
-    //                std::forward<Args>(args)...)
-    //            );
-    //    return task;
-    //}
+		_queue.push_back(wrappedTask);
+		cv.notify_one();
+		return task->get_future(); ;
+	}
 };
 #endif
 
