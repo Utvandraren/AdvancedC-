6925772ed2fcc1697585a9aece9f7407339ff0b3
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2021-04-15 00:35:46 +0200

    added a large part of the code necessary

--- AdvancedC++.cpp
+++ AdvancedC++.cpp
@@ -1,20 +1,30 @@
 // AdvancedC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+// _DEBUG
+
+#include <cassert>
+
 #include <iostream>
+#include "SharedPtr.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+#ifdef DBG_NEW
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	assert(!testPtr);
+	SharedPtr<std::string> testPtr = SharedPtr<std::string>();
+	assert(testPtr);
+	new int;
+
+
+	std::cout << "Code finished!" << std::endl;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ SharedPtr.hpp
@@ -0,0 +1,156 @@
+
+
+
+
+
+template<class T>
+class SharedPtr 
+{
+	T* ptr;
+	int* counter;
+
+public:
+	~SharedPtr(){
+		if (*counter == 1) {
+			delete ptr;
+			delete counter;
+			ptr = nullptr;
+			counter = nullptr;
+		}
+		else
+		{
+			ptr = nullptr;
+			counter = nullptr;
+			--counter;
+		}
+	}
+
+	SharedPtr(){
+		ptr = nullptr;
+		counter = new int(1);
+	}
+
+	explicit SharedPtr(T* ptr){
+		this.ptr = ptr;
+		counter = new int(1);
+	}
+
+	SharedPtr(const std::nullptr_t ptr) {
+		this->ptr = ptr;
+		counter = new int(1);
+	}
+
+	SharedPtr(const SharedPtr& rhs) {
+		ptr = rhs.ptr;
+		counter = rhs.counter;
+		++*counter;
+	}
+
+	SharedPtr(const SharedPtr&& rhs) {
+		ptr = rhs.ptr;
+		counter = rhs.counter;
+		rhs.ptr = nullptr;
+		rhs.counter = new int(1);
+	}
+
+	SharedPtr& operator=(const SharedPtr& other) {
+		if (ptr == other.ptr) {
+			return *this;
+        }
+		
+		delete[] ptr;
+		ptr = other.ptr;
+		counter = other.counter;
+		++* counter;
+		return *this;
+	}
+
+	T& operator*() {   
+		return *ptr;
+	}
+
+	T* operator->() {     
+		return ptr;
+	}
+
+	explicit operator bool() const{
+		return ptr != nullptr;
+	}
+
+	friend auto operator<=>(const SharedPtr& lhs, const SharedPtr& rhs) {
+		auto lIt = lhs.ptr; auto rIt = rhs.ptr;		
+		if (*lIt < *rIt)
+			return std::strong_ordering::less;
+		else if (*lIt > *rIt)
+			return std::strong_ordering::greater;
+		else
+			return std::strong_ordering::equivalent;
+	}
+
+	friend auto operator<=>(const SharedPtr& lhs, const std::nullptr_t rhs) {
+		auto lIt = lhs.ptr; auto rIt = rhs.ptr;
+		if (*lIt < *rIt)
+			return std::strong_ordering::less;
+		else if (*lIt > *rIt)
+			return std::strong_ordering::greater;
+		else
+			return std::strong_ordering::equivalent;
+	}
+
+	bool operator==(const SharedPtr& rhs) {
+		return (this <=> rhs) == 0;
+	}
+	bool operator==(const std::nullptr_t rhs) {
+		return (this <=> rhs) == 0;
+	}
+	bool operator!=(const SharedPtr& rhs) {		
+		return (this <=> rhs) != 0;
+	}
+	bool operator!=( const std::nullptr_t rhs) {
+		return (this <=> rhs) != 0;
+	}
+
+	void reset() {
+		delete ptr;
+		ptr = nullptr;
+	}
+
+	T* get() const{
+		return ptr;
+	}
+
+	size_t use_count() {
+		return *counter;
+	}
+
+
+	bool Invariant() {
+
+	}
+
+
+
+
+
+
+
+
+#pragma region nonmembers
+
+	template<class T>
+	friend void swap(SharedPtr<T>& lhs, SharedPtr<T>& rhs)noexcept
+	{
+
+	};
+
+	template<class T, class...Args>
+	friend SharedPtr<T>MakeShared(Args&&...args)
+	{
+
+	}
+
+	
+#pragma endregion nonmembers
+
+
+};
